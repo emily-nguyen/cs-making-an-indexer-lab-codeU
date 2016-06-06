0fdd3c72e1e8e4fd12679926d22afae89c2b16f3
emily-nguyen <emily-nguyen@users.noreply.github.com>
2016-06-06 14:13:28 -0700

    Implemented size() for TermCounter and indexPage() for Index

--- javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
@@ -36,8 +36,14 @@ public class TermCounter {
 	 * @return
 	 */
 	public int size() {
-        // TODO: fill this in.
-		return -1;
+		int total = 0; 
+
+		for (String key: keySet()) {
+			Integer count = get(key);
+			total += count;
+		}			
+
+		return total; 
 	}
 
 	/**
